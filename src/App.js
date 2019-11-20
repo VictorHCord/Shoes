4d846a8695a5d12bc829fd811821631895f5014e
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-11-20 19:58:31 -0300

    Initial rocketshoes flux

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Redux</h1>
     </div>
   );
 }
-
 export default App;
