9248b7b97315c5441f3b1c5c298674b2105a52cb
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-11-21 15:42:21 -0300

    Styled Home and cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { FaPlusCircle, FaMinusCircle, FaTrash } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+                alt="Tênis"
+              />
+            </td>
+
+            <td>
+              <strong>Tênis</strong>
+              <span>R$1684,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <FaMinusCircle size={20} color="#0089f0" />
+                </button>
+                <input type="number" name="" readOnly value={2} />
+                <button type="button">
+                  <FaPlusCircle size={20} color="#0089f0" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$3.369,8</strong>
+            </td>
+            <td>
+              <button type="button">
+                <FaTrash size={20} color="#0089f0" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1569,60</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,100 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { FaShoppingBasket } from 'react-icons/fa';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Hello Cart</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-sneaker-meia-leve-calce-facil-vr/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <FaShoppingBasket size={16} color="#fff" />3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
